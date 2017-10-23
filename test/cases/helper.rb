eb42a7719e2dba775d6fd2b8ca68e78cc7eed395
lulalala <mark@goodlife.tw>
2017-10-23 17:03:50 +0800

    Copy test from Rails

---
+++ test/cases/helper.rb
@@ -0,0 +1,23 @@
+require "active_model"
+
+# Show backtraces for deprecated behavior for quicker cleanup.
+ActiveSupport::Deprecation.debug = true
+
+# Disable available locale checks to avoid warnings running the test suite.
+I18n.enforce_available_locales = false
+
+require "active_support/testing/autorun"
+require "active_support/testing/method_call_assertions"
+
+class ActiveModel::TestCase < ActiveSupport::TestCase
+  include ActiveSupport::Testing::MethodCallAssertions
+
+  # Skips the current run on Rubinius using Minitest::Assertions#skip
+  private def rubinius_skip(message = "")
+    skip message if RUBY_ENGINE == "rbx"
+  end
+  # Skips the current run on JRuby using Minitest::Assertions#skip
+  private def jruby_skip(message = "")
+    skip message if defined?(JRUBY_VERSION)
+  end
+end
