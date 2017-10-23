83689c634c3763db98ba08940f28d921ed0a9b30
lulalala <mark@goodlife.tw>
2017-10-23 17:04:19 +0800

    Errors class

---
+++ lib/adequate_errors/errors.rb
@@ -0,0 +1,54 @@
+require 'active_model/errors'
+require 'forwardable'
+require 'adequate_errors/error'
+
+module AdequateErrors
+  class Errors
+    extend Forwardable
+    def_delegators :@legacy_errors, *Enumerable.instance_methods(false)
+    def_delegators :@legacy_errors,
+                   :initialize_dup, :copy!, :[], :each, :size, :values, :keys,
+                   :empty?, :blank?, :to_xml, :as_json, :to_hash, :added?, :full_messages, :full_messages_for,
+                   :full_message, :generate_message, :marshal_dump, :marshal_load, :init_with
+
+    def initialize(base)
+      # Reference to Rails official errors implementation
+      @legacy_errors = ::ActiveModel::Errors.new(base)
+
+      @base = base
+      @errors = []
+    end
+
+    def clear
+      @errors.clear
+      @legacy_errors.clear
+    end
+
+    def delete(key)
+      @errors.delete_if do |error|
+        error.attribute == key
+      end
+      @legacy_errors.delete(key)
+    end
+
+    def add(attribute, message = :invalid, options = {})
+      @errors.append(::AdequateErrors::Error.new(@base, attribute, message, options))
+
+      @legacy_errors.add(attribute, message, options)
+    end
+
+    # TODO: Future plan is to make this class enumerate @errors directly.
+    # So when adding new public methods, avoid name collision with Enumerable methods.
+    def error_objects
+      @errors
+    end
+
+    def match(params)
+      return @errors.dup if params.nil?
+
+      @errors.select {|error|
+        error.match?(params)
+      }
+    end
+  end
+end
---
+++ test/cases/adequate_errors/errors_test.rb
@@ -0,0 +1,52 @@
+require "minitest/autorun"
+require "active_model"
+require "models/topic"
+require 'adequate_errors/errors'
+
+describe AdequateErrors::Errors do
+  let(:model) { Topic.new }
+  let(:legacy_errors) { ActiveModel::Errors.new(model) }
+  subject { AdequateErrors::Errors.new(model, legacy_errors: legacy_errors) }
+
+  describe '#add' do
+    it 'assigns attributes' do
+      assert_equal 0, subject.error_objects.size
+      assert_equal 0, legacy_errors.details.count
+
+      subject.add(:title, :not_attractive)
+
+      assert_equal 1, subject.error_objects.size
+      assert_equal :title, subject.error_objects.first.attribute
+      assert_equal :not_attractive, subject.error_objects.first.type
+
+      assert_equal 1, legacy_errors.details.count
+      assert_equal [{:error=>:not_attractive}], legacy_errors.details[:title]
+    end
+  end
+
+  describe '#delete' do
+    it 'assigns attributes' do
+      subject.add(:title, :not_attractive)
+      subject.add(:content, :too_vague)
+
+      subject.delete(:title)
+
+      assert_equal 1, subject.error_objects.size
+      assert_equal 1, legacy_errors.details.count
+      assert_equal [], legacy_errors.details[:title]
+    end
+  end
+
+  describe '#clear' do
+    it 'assigns attributes' do
+      subject.add(:title, :not_attractive)
+      subject.add(:content, :too_vague)
+
+      subject.clear
+
+      assert_equal 0, subject.error_objects.size
+      assert_equal 0, legacy_errors.details.count
+      assert_equal [], legacy_errors.details[:title]
+    end
+  end
+end
