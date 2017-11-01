cc2c449a383f1c0be90748bba4df3ca28c72a3eb
lulalala <mark@goodlife.tw>
2017-11-01 11:49:30 +0800

    Add locale file with attribute moved from overall format to each individual message

--- lib/adequate_errors.rb
+++ lib/adequate_errors.rb
@@ -1,2 +1,6 @@
 require "adequate_errors/version"
 require "adequate_errors/interceptor"
+
+ActiveSupport.on_load(:i18n) do
+  I18n.load_path << File.dirname(__FILE__) + "/adequate_errors/locale/en.yml"
+end
