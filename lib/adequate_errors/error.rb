2cb03d9c3006413d6cb567b5bc5b5ef5a4795e79
lulalala <mark@goodlife.tw>
2017-11-01 12:58:59 +0800

    Implement Error#message
    Based on Rails' `generate_message`

--- lib/adequate_errors/error.rb
+++ lib/adequate_errors/error.rb
@@ -9,11 +9,88 @@ module AdequateErrors
 
     attr_reader :base,:attribute, :type, :options
 
-    def full_message
-      attr_name = @attribute.to_s.tr(".", "_").humanize
-      attr_name = @base.class.human_attribute_name(@attribute, default: attr_name)
-      I18n.t(:"errors.format",
-             attribute: attr_name)
+    # Full message of the error.
+    #
+    # Key difference to Rails errors include:
+    #
+    # 1. Flexible positioning of attribute name interpolation
+    #
+    # In Rails, errors' full messages are always prefixed with attribute name,
+    # and if prefix is not wanted, developer often adds error to the `base' attribute instead.
+    # This can be unreasonable in different languages or special business requirements.
+    #
+    # AdequateErrors leaves the attribute placement to the developer.
+    # For each error message in the locale file, the %{attribute} indicates placement of the attribute.
+    # The same error can have prefix in English, and be prefix-less in Russian.
+    # If no prefix is needed, one should update the locale file accordingly,
+    #
+    # 2. Message evaluated lazily
+    #
+    # In Rails, error message is evaluated during the `add` call.
+    # AdequateErrors evaluates message lazily at `message` call instead,
+    # so one can change message locale after model has been validated.
+    #
+    #
+    #
+    # Order of I18n lookup:
+    #
+    # Error messages are first looked up in <tt>activemodel.adequate_errors.models.MODEL.attributes.ATTRIBUTE.MESSAGE</tt>,
+    # if it's not there, it's looked up in <tt>activemodel.adequate_errors.models.MODEL.MESSAGE</tt> and if
+    # that is not there also, it returns the translation of the default message
+    # (e.g. <tt>activemodel.errors.messages.MESSAGE</tt>). The translated model
+    # name, translated attribute name and the value are available for
+    # interpolation.
+    #
+    # When using inheritance in your models, it will check all the inherited
+    # models too, but only if the model itself hasn't been found. Say you have
+    # <tt>class Admin < User; end</tt> and you wanted the translation for
+    # the <tt>:blank</tt> error message for the <tt>title</tt> attribute,
+    # it looks for these translations:
+    #
+    # * <tt>activemodel.adequate_errors.models.admin.attributes.title.blank</tt>
+    # * <tt>activemodel.adequate_errors.models.admin.blank</tt>
+    # * <tt>activemodel.adequate_errors.models.user.attributes.title.blank</tt>
+    # * <tt>activemodel.adequate_errors.models.user.blank</tt>
+    # * any default you provided through the +options+ hash (in the <tt>activemodel.adequate_errors</tt> scope)
+    # * <tt>activemodel.adequate_errors.messages.blank</tt>
+    # * <tt>adequate_errors.attributes.title.blank</tt>
+    # * <tt>adequate_errors.messages.blank</tt>
+    def message
+      if @options[:message].is_a?(Symbol)
+        type = @options.delete(:message) if @options[:message].is_a?(Symbol)
+      else
+        type = @type
+      end
+
+      if @base.class.respond_to?(:i18n_scope)
+        defaults = @base.class.lookup_ancestors.map do |klass|
+          [ :"#{@base.class.i18n_scope}.adequate_errors.models.#{klass.model_name.i18n_key}.attributes.#{attribute}.#{type}",
+            :"#{@base.class.i18n_scope}.adequate_errors.models.#{klass.model_name.i18n_key}.#{type}" ]
+        end
+      else
+        defaults = []
+      end
+
+      defaults << :"#{@base.class.i18n_scope}.adequate_errors.messages.#{type}" if @base.class.respond_to?(:i18n_scope)
+      defaults << :"adequate_errors.attributes.#{attribute}.#{type}"
+      defaults << :"adequate_errors.messages.#{type}"
+
+      defaults.compact!
+      defaults.flatten!
+
+      key = defaults.shift
+      defaults = @options.delete(:message) if @options[:message]
+      value = (attribute != :base ? @base.send(:read_attribute_for_validation, attribute) : nil)
+
+      i18n_options = {
+        default: defaults,
+        model: @base.model_name.human,
+        attribute: humanized_attribute,
+        value: value,
+        object: @base
+      }.merge!(@options)
+
+      I18n.translate(key, i18n_options)
     end
 
     def match?(params)
@@ -33,5 +110,13 @@ module AdequateErrors
 
       true
     end
+
+    private
+
+    def humanized_attribute
+      default = @attribute.to_s.tr(".", "_").humanize
+      @base.class.human_attribute_name(@attribute, default: default)
+    end
+
   end
 end
--- test/cases/adequate_errors/error_test.rb
+++ test/cases/adequate_errors/error_test.rb
@@ -1,5 +1,7 @@
+require "cases/helper"
 require "minitest/autorun"
-require 'adequate_errors/error'
+require 'adequate_errors'
+require "models/topic"
 
 describe AdequateErrors::Error do
   subject { AdequateErrors::Error.new(self, :mineral, :not_enough, count: 2) }
@@ -34,6 +36,44 @@ describe AdequateErrors::Error do
     it 'handles extra options match' do
       assert_equal false,subject.match?(:count => 1)
       assert_equal true,subject.match?(:count => 2)
+    end
+  end
+
+  describe '#message' do
+    let(:model) { Topic.new }
+
+    it 'returns message' do
+      subject = AdequateErrors::Error.new(model, :title, :inclusion,value: 'title')
+      assert_equal "Title is not included in the list", subject.message
+    end
+
+    it 'returns custom message with interpolation' do
+      subject = AdequateErrors::Error.new(model, :title, :inclusion,message: "custom message %{value}", value: "title")
+      assert_equal "custom message title", subject.message
+    end
+
+    it 'returns plural interpolation' do
+      subject = AdequateErrors::Error.new(model, :title, :too_long, count: 10)
+      assert_equal "Title is too long (maximum is 10 characters)", subject.message
+    end
+
+    it 'returns singular interpolation' do
+      subject = AdequateErrors::Error.new(model, :title, :too_long, count: 1)
+      assert_equal "Title is too long (maximum is 1 character)", subject.message
+    end
+
+    it 'returns count interpolation' do
+      subject = AdequateErrors::Error.new(model, :title, :too_long, message: "custom message %{count}", count: 10)
+      assert_equal "custom message 10", subject.message
+    end
+
+    it 'renders lazily using current locale' do
+      I18n.backend.store_translations(:pl,{adequate_errors: {messages: {invalid: "%{attribute} jest nieprawidłowe"}}})
+
+      I18n.with_locale(:en) { model.errors.adequate.add(:title, :invalid) }
+      I18n.with_locale(:pl) {
+        assert_equal 'Title jest nieprawidłowe', model.errors.adequate.first.message
+      }
     end
   end
 end
